d946a9d64e98c1f40edd8274fb42683c72340415
Derek Alia <derekalia@me.com>
2017-03-09 16:06:53 -0800

    (feat) Sync client state with checkbox test

--- server/server.js
+++ server/server.js
@@ -5,12 +5,37 @@ const mongoose = require('mongoose');
 const socket = require('socket.io');
 const path = require('path');
 
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
 const PORT = process.env.PORT || 3000;
 
-var app = express();
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/test');
+
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../public')));
 app.use('/bundle', express.static(path.join(__dirname, '../bundle')));
 
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('channel-name', (message) => console.log('we got the message', message));
+  socket.on('checked', () => { 
+    console.log('we got here'); 
+    io.clients(function(err, clients) {
+      if (err) { throw err; }
+      console.log(clients);
+    });
+    socket.broadcast.emit('testCheck', {for: 'everyone'});        
+  });
+  
 
-app.listen(PORT, () => (console.log('we are listening on port', PORT)) );
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+});
+
+
+server.listen(PORT, () => (console.log('we are listening on port', PORT)) );
--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SocketIOClient from 'socket.io-client';
+
+var socket = SocketIOClient('http://localhost:3000');
 
 class Index extends React.Component {
   constructor(props) {
     super(props);
-
+    // socket = SocketIOClient('http://localhost:3000');
+    this.state = {
+      isGoing: false,
+    };
+    socket.on('testCheck', ()=>{ console.log('we are over here'); this.handleCheck(); });
   }
+
+  handleCheck() {      
+    console.log('hi', this.state.isGoing);
+    this.setState({
+      isGoing: !this.state.isGoing
+    });
+  }
+
+
 
   render() {
     return (
       <div>
-       <p> This is working</p>
+        <p> This is working</p>
+        <button onClick={() => { socket.emit('channel-name', 'Hello world!'); }}>Emit Me</button>
+        <input type='checkbox' checked={this.state.isGoing} onChange={() => { console.log(this.state.isGoing); socket.emit('checked'); this.handleCheck(); }}></input>
       </div>
     );
   }
 }
 
-
-ReactDOM.render(<Index/>, document.getElementById('root'));
+ReactDOM.render(<Index />, document.getElementById('root'));
 
 
 
