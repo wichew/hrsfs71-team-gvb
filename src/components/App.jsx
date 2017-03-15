352899ca3cee9734dfc186d6bd37f05a9737bdda
Lindsay Anchors <lindsay.anchors@gmail.com>
2017-03-14 19:06:37 -0700

    (feat)Implement routing (bug in opening new 2nd tab

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import Vote from './Vote.jsx';
 import Signup from './Signup.jsx';
 
 const App = () => {
   return (
-    <div>
-      <p>Good-v-Bad: The Game</p>
-      <Signup />
-      <Vote />
-    </div>
+    <Router>
+      <div>
+        <p>Good-v-Bad: The Game</p>
+        <div>
+          <div><Link to='/signup'>Signup</Link></div>
+          <div><Link to='/vote'>Vote</Link></div>
+        </div>
+        <hr/>
+        <Route path='/signup' component={Signup}/>
+        <Route path='/vote' component={Vote}/>
+      </div>
+    </Router>
   );
 };
 
