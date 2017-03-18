2a97300ffc299aaa9e4900c7b807ab96029da8d1
Lindsay Anchors <lindsay.anchors@gmail.com>
2017-03-18 01:00:41 -0400

    (refactor) users get/post requests (wip)

--- server/db/utils.js
+++ server/db/utils.js
@@ -2,18 +2,23 @@ const User = require('./schemas/users.js');
 
 // Returns null is user already exists.
 // Returns newUser, if user is successfully added to database.
-module.exports.addUser = (username, password) => {
+module.exports.addUser = (username, password, cb) => {
+  console.log('adduser called cb ', cb);
   User.find({name: username}, (err, user) => {
     if (err) {
       throw err;
     } else if (user.length) {
-      return null;
+      console.log('user already in database');
+      console.log('cb', cb);
+      cb(null);
     } else {
+      console.log('user being created');
       User.create({name: username, password: password}, (err, newUser) => {
         if (err) {
           throw err;
         } else {
-          return newUser;
+          console.log('user successfully added:', newUser);
+          cb(newUser);
         }
       });
     }
@@ -21,14 +26,16 @@ module.exports.addUser = (username, password) => {
 };
 
 // Returns null if user does not exist.
-module.exports.findUser = (username, password) => {
+module.exports.findUser = (username, cb) => {
   User.findOne({name: username}, (err, user) => {
     if (err) {
       throw err;
     } else if (!user) {
       console.log('user does not exist');
+      return cb(null);
+    } else {
+      cb(user);
     }
-    return user;
   });
 };
 
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../db/schemas/users.js');
-const addUser = require('../db/utils.js').addUser;
+const dbUtils = require('../db/utils.js');
 
-router.post('/users', (req, res) => {
-  let addUserResult = addUser(req.body.username, req.body.password);
-  if (addUserResult === null) {
-    res.status(501).send('Failed to add to user database');
-  } else {
-    res.status(201).send('User added to database');
-  }
+// router.post('/users', (req, res) => {
+//   dbUtils.addUser(req.body.username, req.body.password, function(user) {
+//     if (!user) {
+//       res.status(501).send('Failed to add to user database');
+//     } else {
+//       res.status(201).send('User added to database');
+//     }
+//   });
+// });
+
+router.get('/users/:username', (req, res) => {
+  dbUtils.findUser(req.params.username, (user) => {
+    if (!user) {
+      res.status(404).send('User does not exist');
+    } else {
+      res.status(200).send(user);
+    }
+  });
 });
 
 module.exports = router;
