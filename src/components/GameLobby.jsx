1e21d941fe295458ecdce3f5cc0f004bba21e920
Lindsay Anchors <lindsay.anchors@gmail.com>
2017-03-18 19:07:49 -0400

    WIP

--- src/components/GameLobby.jsx
+++ src/components/GameLobby.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
-import { Link, Route, Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import SocketIOClient from 'socket.io-client';
+import Player from './Player.jsx';
 
-// Not currently in use
+const socket = SocketIOClient('http://localhost:3000');
+const MIN_PLAYERS = 5;
+
 class GameLobby extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      players: []
+    };
+    socket.on('setPlayerID', (id) => { this.setState({ players: [...this.state.players, id] }); });
   }
 
   render () {
-    return (
+    if (players.length === MIN_PLAYERS) {
+      return (
         <div>
-          <Link to='/game/vote'>Force Start</Link>
+          { this.state.players.map((player) => <div>{player}</div>) }
+          <Link to='/game/vote'>Start</Link>
+          <Route path='/game/vote' render={ ()=> <Game socket={socket} /> }/>
         </div>
-    );
+      );
+    } else {
+      return (
+        <div>
+          <div>Waiting for all players to join . . .</div>
+          { this.state.players.map((player) => <div>{player}</div>) }
+        </div>
+      );
+    }
   }  
 }
 
