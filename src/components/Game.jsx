4d68a84b990329ca47d412db0d6038ab43167c9b
Lindsay Anchors <lindsay.anchors@gmail.com>
2017-03-18 21:15:45 -0400

    refactor/GameLobby back in separate component

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SocketIOClient from 'socket.io-client';
 import Player from './Player.jsx';
+import GameLobby from './GameLobby.jsx';
 
 var socket = SocketIOClient('http://localhost:3000');
 const MIN_PLAYERS = 5;
@@ -67,12 +68,7 @@ class Game extends React.Component {
   render() {
     if (this.state.resultsArray.length < MIN_PLAYERS) {
       return (
-        <div>
-          <div>Waiting for all players to join . . .</div>
-          { this.state.resultsArray.map((player, i) => {
-            return <Player selected={player.selected} isPicker={this.isPicker} roundVote={this.roundVote} vote={player.vote} handleCheck={this.handleCheck} key={player.key} userID={player.userID} pickerID={this.state.picker} />;
-          })}
-        </div>
+        < GameLobby gameState={this.state} />
       );
     } else {
       return (
--- src/components/GameLobby.jsx
+++ src/components/GameLobby.jsx
@@ -1,37 +1,22 @@
-/*import React from 'react';
-import { Link } from 'react-router-dom';
-import SocketIOClient from 'socket.io-client';
+import React from 'react';
 import Player from './Player.jsx';
-import Game from './Game.jsx';
-
-const socket = SocketIOClient('http://localhost:3000');
-const MIN_PLAYERS = 5;
 
 class GameLobby extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      resultsArray: []
-    };
-    socket.on('updateArray', (array) => { this.setState({ resultsArray: array }); console.log('Array Updated To:', this.state.resultsArray); });
+  constructor({gameState}) {
+    super({gameState});
+    this.state = gameState;
   }
 
   render () {
-    if (this.state.resultsArray.length >= MIN_PLAYERS) {
-      return (
-        <div>
-          <Game socket={socket} />
-        </div>
-      );
-    } else {
-      return (
-          <div>
-            <div>Waiting for all players to join . . .</div>
-            { this.state.resultsArray.map((player, i) => <Player key={i} playerStats={player} />) }
-          </div>
-      );
-    }
+    return (
+      <div>
+        <div>Waiting for all players to join . . .</div>
+          { this.state.resultsArray.map((player, i) => {
+            return <Player selected={player.selected} isPicker={this.isPicker} roundVote={this.roundVote} vote={player.vote} handleCheck={this.handleCheck} key={player.key} userID={player.userID} pickerID={this.state.picker} />;
+          })}
+      </div>
+    );
   }  
 }
 
-export default GameLobby;*/
+export default GameLobby;
