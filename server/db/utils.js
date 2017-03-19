55d54f875068cc1e5070db2c1bae8a0c64e23ec8
Aidan Bane <aidanfbane@gmail.com>
2017-03-18 17:41:46 -0700

    Login.jsx is now sending POST request to login

--- server/db/utils.js
+++ server/db/utils.js
@@ -36,23 +36,23 @@ module.exports.findUser = (username, cb) => {
 };
 
 module.exports.checkUserPassword = (username, password, cb) => {
-    User.findOne({name: username}, (err, user) => {
-      if (err) {
-        throw err;
-      } else if (!user) {
-        console.log('user does not exist');
-        return cb(null);
-      } else {
-        return user;
-      }
-    })
-    .then((user) => {
-      if (user.password === password) {
-        console.log('PASSWORDS MATCH: ', 'Pword form mongo', user.password, 'Pword form POST', password);
-      } else {
-        console.log('PASSWORDS DO NOT MATCH: ', 'Pword form mongo', user.password, 'Pword form POST', password);
-      }
-    });
+  User.findOne({name: username}, (err, user) => {
+    if (err) {
+      throw err;
+    } else if (!user) {
+      console.log('user does not exist');
+      return cb(null);
+    } else {
+      return user;
+    }
+  })
+  .then((user) => {
+    if (user.password === password) {
+      console.log('PASSWORDS MATCH: ', 'Pword form mongo', user.password, 'Pword form POST', password);
+    } else {
+      console.log('PASSWORDS DO NOT MATCH: ', 'Pword form mongo', user.password, 'Pword form POST', password);
+    }
+  });
 };
 
 
